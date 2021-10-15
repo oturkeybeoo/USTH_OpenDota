fb886f5181050064cd52a8455868e0c9a3b47cb9
Benngu235 <nguyenanhquan2801@gmail.com>
2021-10-16 02:14:44 +0700

    Done Home Tab with 4/4 pages

---
+++ app/src/main/java/vn/edu/usth/opendota/HomePeersFragment.java
@@ -0,0 +1,29 @@
+package vn.edu.usth.opendota;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomePeersFragment extends Fragment {
+
+    public HomePeersFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_peers, container, false);
+    }
+}
--- app/src/main/java/vn/edu/usth/opendota/MainActivity.java
+++ app/src/main/java/vn/edu/usth/opendota/MainActivity.java
@@ -11,13 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AddViewPager();
+    }
+
+    private void AddViewPager() {
         ViewPager viewPager = findViewById(R.id.viewpager);
         // setting up the adapter
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
         // add the fragments
-        viewPagerAdapter.add(new HomeOverviewFragment(), "HANOI, VIETNAM");
-        viewPagerAdapter.add(new HomeOverviewFragment(), "PARIS, FRANCE");
+        viewPagerAdapter.add(new HomeOverviewFragment(), "Overview");
+        viewPagerAdapter.add(new HomeMatchesFragment(), "Matches");
+        viewPagerAdapter.add(new HomeHeroesFragment(), "Heroes");
+        viewPagerAdapter.add(new HomePeersFragment(), "Peers");
 
 
         // Set the adapter
