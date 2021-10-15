d98cbcfaf153ca70ab511a8b8908224f5c728eae
Benngu235 <nguyenanhquan2801@gmail.com>
2021-10-15 22:21:31 +0700

    New static colors, roundedLayout, roundedImage, Home/OverviewPage

--- app/src/main/java/vn/edu/usth/opendota/MainActivity.java
+++ app/src/main/java/vn/edu/usth/opendota/MainActivity.java
@@ -1,6 +1,7 @@
 package vn.edu.usth.opendota;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        // setting up the adapter
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // add the fragments
+        viewPagerAdapter.add(new HomeOverviewFragment(), "HANOI, VIETNAM");
+        viewPagerAdapter.add(new HomeOverviewFragment(), "PARIS, FRANCE");
+
+
+        // Set the adapter
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
