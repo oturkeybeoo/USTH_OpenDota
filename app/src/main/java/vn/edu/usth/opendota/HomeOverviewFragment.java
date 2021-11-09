732df30d691763ebe7f9b36d23c784e3d75faa48
Vinh Nguyen <oturkeybeoo@gmail.com>
2021-11-09 14:01:46 +0700

    done api for home overview

--- app/src/main/java/vn/edu/usth/opendota/HomeOverviewFragment.java
+++ app/src/main/java/vn/edu/usth/opendota/HomeOverviewFragment.java
@@ -10,19 +10,31 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 import vn.edu.usth.opendota.adapter.MatchAdapter;
+import vn.edu.usth.opendota.databinding.FragmentHomeOverviewBinding;
+import vn.edu.usth.opendota.databinding.FragmentMatchOverviewBinding;
 import vn.edu.usth.opendota.model.MatchModel;
 
 public class HomeOverviewFragment extends Fragment {
 
+    private FragmentHomeOverviewBinding binding;
+
     public HomeOverviewFragment() {
         // Required empty public constructor
     }
-
-    private RecyclerView recyclerView;
-    ArrayList<MatchModel> matchList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,26 +44,52 @@ public class HomeOverviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home_overview, container, false);
-        recyclerView = view.findViewById(R.id.recentmatch);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        binding = FragmentHomeOverviewBinding.inflate(inflater, container, false);
 
-        recyclerView.setAdapter(new MatchAdapter(getContext(), initData()));
+        View view = binding.getRoot();
+        fetchData();
         return view;
     }
 
-    private ArrayList<MatchModel> initData() {
-        matchList = new ArrayList<>();
-        matchList.add(new MatchModel(R.drawable.dire_logo,"All Draft", "2/6/2", "26.43", "5 days"));
-        matchList.add(new MatchModel(R.drawable.dire_logo,"All Draft", "2/6/2", "26.43", "5 days"));
+    private void fetchData() {
+        String match_id = "339941742";
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String url = String.format("https://opendota.com/api/players/%s", match_id);
 
-        return matchList;
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            Integer leaderboard_rank = response.getInt("leaderboard_rank");
+                            Integer solo_competitive_rank = response.getInt("solo_competitive_rank");
+                            Integer competitive_rank = response.getInt("competitive_rank");
+                            Integer mmr = response.getJSONObject("mmr_estimate").getInt("estimate");
+                            String name = response.getJSONObject("profile").getString("personaname");
+                            String avatar = response.getJSONObject("profile").getString("avatar");
+
+                            binding.homeOverviewName.setText(name);
+                            binding.leaderboardRank.setText(String.format("#%d",leaderboard_rank));
+                            binding.soloCompetitiveRank.setText(String.format("#%d",solo_competitive_rank));
+                            binding.competitiveRank.setText(String.format("#%d",competitive_rank));
+                            binding.mmr.setText(mmr.toString());
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(jsonObjectRequest);
     }
 
-//    @Override
-//    public void onItemClicked() {
-//        Intent intent = new Intent(getActivity(), MatchDetailsActivity.class);
-//        startActivity(intent);
-//    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
 }
